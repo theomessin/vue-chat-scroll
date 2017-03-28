e111304a65e2b9b14e4f2c5b028e9546987736cc
Theodore Messinezis <theo@theomessin.com>
2017-03-28 16:41:00 +0100

    Revert "Fix webpack build config (#9)"
    This reverts commit 6ec8d8e3a76448645fb7c1955f7101183fae1a85.

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,9 +14,3 @@ let mix = require('laravel-mix');
 mix.js('src/vue-chat-scroll.js', './')
    .js('src/example/main.js', './example/')
    .setPublicPath('dist/');
-
-mix.webpackConfig({
-  output: {
-    libraryTarget: 'umd'
-  }
-})
