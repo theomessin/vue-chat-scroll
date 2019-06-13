dff1afe86e62703a91473d3dd53dbe12ddfbbc43
kstudnicki <krzysztof.studnicki@comarch.com>
2019-06-13 16:00:01 +0200

    Fixed scrolling with more than one mutation record
    I have a logic, where styles of the message bubbles are formatted like on
    facebook - first bubble is rounded everywhere, than next bubble causes
    first to change it's corner roundness, etc.
    Because of that, there were multiple MutationRecord objects in "e"
    object and only the first was the added node. Now your code checks
    the last MutationRecord, and it was only the removed node.
    I don't know if it will be universal to all cases, but maybe consider
    adjustable index of the "e" array?

--- src/directives/v-chat-scroll.js
+++ src/directives/v-chat-scroll.js
@@ -28,9 +28,9 @@ const vChatScroll = {
       let config = binding.value || {};
       let pause = config.always === false && scrolled;
       if (config.scrollonremoved) {
-        if (pause || e[e.length - 1].addedNodes.length != 1 && e[e.length - 1].removedNodes.length != 1) return;
+        if (pause || e[0].addedNodes.length != 1 && e[e.length - 1].removedNodes.length != 1) return;
       } else {
-        if (pause || e[e.length - 1].addedNodes.length != 1) return;
+        if (pause || e[0].addedNodes.length != 1) return;
       }
       scrollToBottom(el, config.smooth);
     })).observe(el, { childList: true, subtree: true });
