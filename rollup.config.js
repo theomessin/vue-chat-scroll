3fa95ac467c3830e216e4d1ebedd50e0d6674f80
Theodore Messinezis <theodore@messinezis.com>
2020-04-07 21:21:12 +0100

    build: add rollup with typescript support

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    name: 'vue-chat-scroll',
+    format: 'umd',
+  },
+  plugins: [
+    typescript(),
+  ],
+};
