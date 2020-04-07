40e8a785f6cbcc7269cd505821cd9b1d62da1d1b
Theodore Messinezis <theodore@messinezis.com>
2020-04-07 22:30:42 +0100

    feat: implement minimum viable plugin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-//
+import { DirectiveOptions, PluginObject } from 'vue';
+
+const directive: DirectiveOptions = {
+  bind: (el) => {
+    const observer = new MutationObserver((e) => {
+      const nodesWereAdded = e[e.length - 1].addedNodes.length === 1;
+      const nodesWereRemoved = e[e.length - 1].removedNodes.length === 1;
+      if (nodesWereAdded === false && nodesWereRemoved === false) return;
+      el.scroll({ top: el.scrollHeight });
+    });
+
+    observer.observe(el, {
+      childList: true,
+      subtree: true,
+    });
+  },
+};
+
+const plugin: PluginObject<any> = {
+  install: (Vue) => {
+    Vue.directive('chat-scroll', directive);
+  },
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(plugin);
+}
+
+export default plugin;
