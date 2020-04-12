01ed28af55d7f58bf3573490d7829f5774162418
Theodore Messinezis <theodore@messinezis.com>
2020-04-12 01:53:01 +0100

    feat: add basic enable configuration

--- tests/directive.spec.ts
+++ tests/directive.spec.ts
@@ -3,7 +3,6 @@ import { directive } from '../src/directive';
 import { scroll } from '../src/scroll';
 
 jest.mock('../src/scroll');
-// Clear all mocks before each test.
 beforeEach(() => { jest.clearAllMocks(); });
 
 test('it automatically calls scrolls on init', () => {
@@ -11,8 +10,9 @@ test('it automatically calls scrolls on init', () => {
     template: '<div v-chat-scroll />',
   };
 
+  const localDirective = directive;
   const localVue = vueTestUtils.createLocalVue();
-  localVue.directive('chat-scroll', directive);
+  localVue.directive('chat-scroll', localDirective);
 
   const wrapper = vueTestUtils.mount(Component, { localVue });
   expect(scroll).toHaveBeenCalledTimes(1);
@@ -25,21 +25,16 @@ test('it calls scroll when an item is added', async () => {
     template: '<div v-chat-scroll><div v-for="item in items">{{ item }}</div></div>',
   };
 
+  const localDirective = directive;
+  localDirective.inserted = jest.fn();
   const localVue = vueTestUtils.createLocalVue();
-  localVue.directive('chat-scroll', directive);
-
+  localVue.directive('chat-scroll', localDirective);
   const wrapper = vueTestUtils.mount(Component, { localVue });
-  expect(scroll).toHaveBeenCalledTimes(1);
-  expect(scroll).toHaveBeenCalledWith(wrapper.element);
 
   (wrapper.vm as any).$data.items.push('A new item');
   await (wrapper.vm as any).$nextTick();
-  expect(scroll).toHaveBeenCalledTimes(2);
-
-  (wrapper.vm as any).$data.items.push('Here\'s two items in a row!');
-  (wrapper.vm as any).$data.items.push('This should call scroll just once!');
-  await (wrapper.vm as any).$nextTick();
-  expect(scroll).toHaveBeenCalledTimes(3);
+  expect(scroll).toHaveBeenCalledTimes(1);
+  expect(scroll).toHaveBeenCalledWith(wrapper.element);
 });
 
 test('it calls scroll when an item is removed', async () => {
@@ -48,19 +43,36 @@ test('it calls scroll when an item is removed', async () => {
     template: '<div v-chat-scroll><div v-for="item in items">{{ item }}</div></div>',
   };
 
+  const localDirective = directive;
+  localDirective.inserted = jest.fn();
   const localVue = vueTestUtils.createLocalVue();
-  localVue.directive('chat-scroll', directive);
-
+  localVue.directive('chat-scroll', localDirective);
   const wrapper = vueTestUtils.mount(Component, { localVue });
+
+  (wrapper.vm as any).$data.items.pop();
+  await (wrapper.vm as any).$nextTick();
   expect(scroll).toHaveBeenCalledTimes(1);
   expect(scroll).toHaveBeenCalledWith(wrapper.element);
+});
+
+test('it obeys the enabled configuration parameter', async () => {
+  const Component: Vue.Component = {
+    data: () => ({ enabled: false, items: [1, 2, 3] }),
+    template: '<div v-chat-scroll="{ enabled }"><div v-for="item in items">{{ item }}</div></div>',
+  };
+
+  const localDirective = directive;
+  const localVue = vueTestUtils.createLocalVue();
+  localVue.directive('chat-scroll', localDirective);
+  const wrapper = vueTestUtils.mount(Component, { localVue });
 
   (wrapper.vm as any).$data.items.pop();
   await (wrapper.vm as any).$nextTick();
-  expect(scroll).toHaveBeenCalledTimes(2);
+  expect(scroll).not.toHaveBeenCalled();
 
-  (wrapper.vm as any).$data.items.pop();
+  (wrapper.vm as any).$data.enabled = true;
   (wrapper.vm as any).$data.items.pop();
   await (wrapper.vm as any).$nextTick();
-  expect(scroll).toHaveBeenCalledTimes(3);
+  expect(scroll).toHaveBeenCalledTimes(1);
+  expect(scroll).toHaveBeenCalledWith(wrapper.element);
 });
