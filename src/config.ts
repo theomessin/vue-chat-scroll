3ae2dfd29c78d00de56cffaad0a0f2ca5189def4
Theodore Messinezis <theodore@messinezis.com>
2020-04-13 05:02:42 +0100

    refactor: add separate config and pass to callback
    Now the Config is passed to the MutationObserver callback instead of it
    having to be resolved using the state object each time. This makes more
    sense as the state is now only used when the directive binding is
    updated.

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+/**
+ * This interface defines the config for the v-chat-scroll directive.
+ */
+export interface Config {
+  enabled: boolean,
+};
+
+/**
+ * This object defines the default config for the directive.
+ */
+export const defaultConfig: Config = {
+  enabled: true,
+};
