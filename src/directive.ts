f1836bc26efe7cce86b7ebef8746270a0f755b1f
Theodore Messinezis <theodore@messinezis.com>
2020-04-22 22:54:50 +0100

    feat: add ability to handle item prepending
    When the handlePrepend config flag is set to true, v-chat-scroll will
    keep track of the changing scrollHeight of the container. When scrollTop
    is zero (ie. the user's scrolled all the way to the top) and items are
    added, v-chat-scroll will scroll to keep the previously viewed item in
    place by scrolling to the difference in scrollHeight before and after
    the new items were added.

--- src/directive.ts
+++ src/directive.ts
@@ -2,20 +2,31 @@ import { DirectiveOptions } from 'vue';
 import { Config, defaultConfig } from './config';
 import { scroll } from './scroll';
 
-/**
- * We use this state to keep track of all v-chat-scroll mutation observers.
- * When a directive binding is updated for an element, we can fetch the
- * existing MutationObserver, disconnect it, and replace the state
- * with the new MutationObserver (with the new Config callback).
- */
-const state: WeakMap<object, MutationObserver> = new WeakMap();
+const observers: WeakMap<Element, MutationObserver> = new WeakMap();
+const heights: WeakMap<Element, number> = new WeakMap();
 
 /**
  * This function is called when a mutation is observed.
  * The config is used to determine what to do.
  */
-const mutationObserved = (el: Element, config: Config) => {
-  if (config.enabled) scroll(el);
+const mutationObserved = (el: Element, config: Config): void => {
+  // if not enabled, do nothing.
+  if (config.enabled === false) return;
+
+  // if not handling prepend, simply scroll.
+  if (config.handlePrepend === false) {
+    scroll(el);
+    return;
+  }
+
+  // if handling prepend, we need to calculate where to scroll to.
+  // We're prepending if scrollTop is zero and heights has the el.
+  // ScrollTop will be difference in scrollHeight before and after.
+  const scrollTop = (el.scrollTop === 0 && heights.has(el))
+    && (el.scrollHeight - heights.get(el));
+
+  scroll(el, scrollTop);
+  heights.set(el, el.scrollHeight);
 };
 
 /**
@@ -29,17 +40,17 @@ export const directive: DirectiveOptions = {
 
   /**
    * When the directive binding is updated we have to update our MutationObserver.
-   * We disconnect the old MutationObserver (if it exists in our previous state).
-   * We then create (and save) a new MutationObserver with the new configuration.
+   * We disconnect the old MutationObserver (if it already exists in observers).
+   * We then create and save a new MutationObserver with the new callback.
    */
   update: (el, binding) => {
-    if (state.has(el)) state.get(el).disconnect();
+    if (observers.has(el)) observers.get(el).disconnect();
     const config: Config = { ...defaultConfig, ...binding.value };
     const mutationCallback: MutationCallback = () => { mutationObserved(el, config); };
 
     const mutationObserver = new MutationObserver(mutationCallback);
     mutationObserver.observe(el, { childList: true, subtree: true });
-    state.set(el, mutationObserver);
+    observers.set(el, mutationObserver);
   },
 };
 
